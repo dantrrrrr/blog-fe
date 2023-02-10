f5f18fa3b17adf649f6628c8cb6cbaee7333da0a
dantr <dantruongeth@gmail.com>
2023-02-10 11:19:12 +0700

    fix user creden

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -22,9 +22,11 @@ const Login = () => {
         try {
             const res = await AxiosRequest.post('/api/auth/login', data,
                 {
-                    headers: { 'Content-Type': 'application/json' }
-                }
+                    headers: { 'Content-Type': 'application/json' },
+
+                }, { withCredentisals: true }
             );
+            console.log(res.data)
             dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
         } catch (error) {
             dispatch({ type: "LOGIN_FAILURE" });
@@ -34,7 +36,7 @@ const Login = () => {
     }
     // console.log("error",errors);
 
-   
+
     user && console.log(user)
     return (
         <div className='login'>
@@ -52,12 +54,12 @@ const Login = () => {
             <form action="" className="loginForm" onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="">Email</label>
                 <input type="text" className='loginInput' placeholder="User Name :" {...register("username", { required: true, max: 32, min: 5, maxLength: 15, pattern: /^[a-z0-9_]{3,32}$/i })} />
-                
+
                 {/* <input type="text" placeholder='Enter username .' className='loginInput' onChange={e => setUsername(e.target.value)} /> */}
                 <label htmlFor="">Password</label>
                 {/* <input type="password" placeholder='Enter Password ' className='loginInput' onChange={e => setPassword(e.target.value)} /> */}
-                <input type="password"className='loginInput'  placeholder="Password : " {...register("password", { required: true, max: 32, min: 1, maxLength: 20 })} />
-              
+                <input type="password" className='loginInput' placeholder="Password : " {...register("password", { required: true, max: 32, min: 1, maxLength: 20 })} />
+
                 <button className='loginButton' disabled={isFetching} >Login</button>
                 {fetchError && (
                     <span className='error'>{fetchError}</span>
